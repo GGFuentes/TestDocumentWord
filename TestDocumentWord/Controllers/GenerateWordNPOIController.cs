6f60a31f85eb112bd0dc7032cae6bab6f36a193b
Gonzales Fuentes <gustavo.gonzales@rpgo.pe>
2021-10-06 17:40:00 -0500

    quitar librerias que no uso

--- TestDocumentWord/Controllers/GenerateWordNPOIController.cs
+++ TestDocumentWord/Controllers/GenerateWordNPOIController.cs
@@ -1,13 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using System.Net;
 using TestDocumentWord.Repositories;
 
 namespace TestDocumentWord.Controllers
